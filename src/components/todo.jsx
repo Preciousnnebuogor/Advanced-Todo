ddd76d0d59cb7014be02c9caefa2e0d692ec9a66
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2025-07-23 23:06:55 -0700

    added

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 
 export default function Todo() {
   const [getValue, setGetValue] = useState("");
-  const [displayValue, setDisplayValue] = useState("");
+  const [activeValue, setActiveValue] = useState("");
   const [allValue, setAllValue] = useState([]);
+  const [check, setCheck] = useState(false)
 
   function handleSubmit() {
     if (!getValue) return;
     // setDisplayValue(getValue);
     setGetValue("");
     const newArr = [...allValue, getValue];
+    
     setAllValue(newArr);
   }
   function deleteValue(index){
@@ -17,8 +19,19 @@ export default function Todo() {
     copy.splice(index, 1)
     setAllValue(copy)
   }
-  
 
+  function activeHandle(){
+     const newTask = {text:getValue, done: false}
+     setActiveValue([...allValue, newTask])
+    // setActiveValue("")
+  }
+  function toggleDone(index) {
+    setAllValue(
+      allValue.map((task, i) =>
+        i === index ? { ...task, done: !task.done } : task
+      )
+    );
+  }
   return (
     <div>
       <div>
@@ -34,12 +47,30 @@ export default function Todo() {
           <button onClick={handleSubmit}>button</button>
         </div>
         <div>
+            <p>All</p>
+            <p>Active</p>
+        </div>
+        <div>
           {allValue.map((data, index) => {
-           return <div>{data}
-             <button onClick={deleteValue}>del</button>
-           </div>;
-           
+            return (
+              <div>
+                <input
+                  type="checkbox"
+                  onChange={(e) => setCheck(e.target.value)}
+                  value={check}
+                />
+                {data}
+                <button onClick={deleteValue}>del</button>
+              </div>
+            );
           })}
+          {allValue.map((data, index) => (
+            <div>
+              <input onChange={() => toggleDone(index)} />
+              <p>{data.done}</p>
+              <button onClick={() => deleteValue(index)}>del</button>
+            </div>
+          ))}
           <p>All</p>
           {/* <p>Active</p>
             <p>Computed</p> */}
